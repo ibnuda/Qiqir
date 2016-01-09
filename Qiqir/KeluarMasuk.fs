7be4a671ad6a3bb4582e8d411800133ad9e5ca1b
Ibnu Daru Aji <ibnu.d.a@student.uns.ac.id>
2016-01-10 04:36:01 +0700

    Init commit

---
+++ Qiqir/KeluarMasuk.fs
@@ -0,0 +1,13 @@
+﻿namespace Qiqir
+
+open System
+open System.Linq
+open SQLite
+
+type KeluarMasuk() =
+    [<PrimaryKey>]
+    [<AutoIncrement>]
+    member val Id: int = 0 with get, set
+
+    member val Amount: double = 0.0 with get, set
+
---
+++ Qiqir/MainActivity.fs
@@ -0,0 +1,25 @@
+﻿namespace Qiqir
+
+open System
+
+open Android.App
+open Android.Content
+open Android.OS
+open Android.Runtime
+open Android.Views
+open Android.Widget
+
+[<Activity (Label = "Qiqir", MainLauncher = true)>]
+type MainActivity () =
+    inherit Activity ()
+
+    let mutable count:int = 1
+
+    override this.OnCreate (bundle) =
+
+        base.OnCreate (bundle)
+
+        // Set our view from the "main" layout resource
+        this.SetContentView (Resource_Layout.Main)
+
+
